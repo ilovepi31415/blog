9a7509bd13a823200c35561af48c8b4e162cf3b3
David Randall <davidra@andrews.edu>
2025-10-06 15:47:07 -0400

    Add textnode unit tests

---
+++ src/test_textnode.py
@@ -0,0 +1,21 @@
+import unittest
+from textnode import TextNode, TextType
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+    
+    def test_neq(self):
+        node = TextNode("This is a text node", TextType.CODE)
+        node2 = TextNode("This is another node", TextType.PLAINTEXT)
+        self.assertNotEqual(node, node2)
+    
+    def test_url(self):
+        node = TextNode('test', TextType.LINK, None)
+        node2 = TextNode('test', TextType.LINK)
+        self.assertEqual(node, node2)
+
+if __name__ == '__main__':
+    unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -10,7 +10,7 @@ class TextType(Enum):
 
 
 class TextNode:
-    def __init__(self, text, text_type: TextType, url):
+    def __init__(self, text, text_type: TextType, url=None):
         self.text = text
         self.text_type = text_type
         self.url = url
