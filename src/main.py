a5f982b628993356f7d7fe66dd63482d1f9aa344
David Randall <davidra@andrews.edu>
2025-10-14 01:46:58 -0400

    Build for gh pages

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from textnode import TextType, TextNode
 from htmlnode import HTMLNode
 from markdown_to_html import markdown_to_html_node
-import os, shutil
+import os, shutil, sys
 
 def copy_tree(src: str, dest: str):
     if (
@@ -28,7 +28,7 @@ def extract_title(markdown: str):
                 return title
     raise Exception('No title in file')
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     if (
         not os.path.exists(from_path) or 
         os.path.isdir(from_path) or
@@ -45,23 +45,25 @@ def generate_page(from_path, template_path, dest_path):
     html = markdown_to_html_node(content).to_html()
     title = extract_title(content)
     new_text = template.replace('{{ Title }}', title).replace('{{ Content }}', html)
+    new_text = new_text.replace('href="/', f'href="{basepath}').replace('src="/', f'src="{basepath}')
     with open(dest_path, 'w') as d:
         d.write(new_text)
 
-def generate_page_recursively(dir_path_content, template_path, dest_dir_path):
+def generate_page_recursively(dir_path_content, template_path, dest_dir_path, basepath):
     for path in os.listdir(dir_path_content):
         if path.endswith('.md'):
-            generate_page(os.path.join(dir_path_content, path), template_path, os.path.join(dest_dir_path, path)[:-2] + 'html')
+            generate_page(os.path.join(dir_path_content, path), template_path, os.path.join(dest_dir_path, path)[:-2] + 'html', basepath)
         else:
             os.makedirs(os.path.join(dest_dir_path, path), exist_ok=True)
-            generate_page_recursively(os.path.join(dir_path_content, path), template_path, os.path.join(dest_dir_path, path))
+            generate_page_recursively(os.path.join(dir_path_content, path), template_path, os.path.join(dest_dir_path, path), basepath)
 
 
 def main():
-    node = TextNode('hello world', TextType.PLAINTEXT, 'www.tacobell.com')
-    print(node)
-    copy_tree('static', 'public')
-    print(extract_title('# howdy'))
-    generate_page_recursively('content', 'template.html', 'public')
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+    else:
+        basepath = '/'
+    copy_tree('static', 'docs')
+    generate_page_recursively('content', 'template.html', 'docs', basepath)
 
 main()
